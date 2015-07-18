76bd35b1011886c0d2df23287e343243ce356be4
Robert Habermann <mail@rhab.de>
2015-07-18 16:15:44 +0200

    start rsync mock tests

--- template/tests/test_misc.py
+++ template/tests/test_misc.py
@@ -18,6 +18,8 @@ sys.path.append(os.path.join(current_path, os.pardir))
 
 # Imports
 import unittest2 as unittest
+from mock import MagicMock
+
 import modules.misc as misc
 
 
@@ -25,6 +27,10 @@ class TestMisc(unittest.TestCase):
 
     def test_say_hello_ok(self):
         self.assertEqual("Hello World", misc.say_hello_world())
+
+    def test_rsync_remote_to_local(self):
+        rsync_remote_to_local = MagicMock(return_value = 3)
+
 
 
 def main():
