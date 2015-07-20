73e7c9a678f5a9eeda36e34fc5feb75dc7d1d68b
Robert Habermann <mail@rhab.de>
2015-07-20 11:16:26 +0200

    bump to 0.6.1

--- template/tests/test_misc.py
+++ template/tests/test_misc.py
@@ -7,7 +7,7 @@
 # Date:         1970-01-01
 
 # Versioning
-__version_info__ = ('0', '6', '0')
+__version_info__ = ('0', '6', '1')
 __version__ = '.'.join(__version_info__)
 
 # make sure (early) that parent dir (main app) is in path
