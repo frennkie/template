0863099043d168f7ae322fa7c52531dfb5bee934
Robert Habermann <mail@rhab.de>
2015-07-25 12:35:12 +0200

    switch from nature to rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 import shlex
 
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 #import alabaster
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -116,10 +116,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
-#html_theme = 'classic'
-html_theme = 'nature'
-#html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+# nature is not responsive
+#html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -128,7 +127,7 @@ html_theme = 'nature'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
